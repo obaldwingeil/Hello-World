5801ac2d2f7f9b2c6c6edca150c671033e38f30b
Olivia Baldwin <obaldwingeil@oxy.edu>
2021-01-21 16:47:30 -0800

    second commit

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,12 +3,55 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    // define variables outside of methods
+    private Button button_hi;
+    private int number = 0;
+    private TextView textView_count;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // part of the android lifecycle
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // links the activity to the xml layout called activity main
+
+        // look up the button by its ID
+        button_hi = findViewById(R.id.button_hello);
+        textView_count = findViewById(R.id.textView_count);
+
+        // add an event listener to listen for the click
+        button_hi.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                // handle what happens after I click
+                sayHello(v);
+            }
+        });
+        // when click happens, do something
+    }
+
+    public void sayHello(View view){
+        // create a toast with a message saying hello
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view){
+        // increment the value of number
+        // set the text of the text view to number
+        number++;
+        // when you see an object, you want to check to make sure it's not null
+        if(textView_count != null){
+            textView_count.setText(Integer.toString(number)); // setText takes a string
+        }
+
     }
 }
